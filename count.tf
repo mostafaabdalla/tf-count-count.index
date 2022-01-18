a04f433e7e03ef66991527b63df35e37b8e350b5
mostafa <mostafaa.abdallaa@gmail.com>
2022-01-18 15:03:40 +0000

    Used count index with list variable

--- count.tf
+++ count.tf
@@ -2,7 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "elb_names" {
+  type    = list(any)
+  default = ["dev.loadbalancer", "stage.loadbalancer", "prod.loadbalancer"]
+}
+
 resource "aws_iam_user" "lb" {
-  name  = "loadbalancer.${count.index}"
+  name  = var.elb_names[count.index]
   count = 3
 }
